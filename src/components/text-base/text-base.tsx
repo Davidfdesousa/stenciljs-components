c6293ab6a31e54775ea87c2bfadaff4bfea01bc7
davidfdesousa <davidfdesousa@hotmail.com>
2025-02-01 14:35:57 -0300

    create function setTheme and create 3 themes

--- src/components/text-base/text-base.tsx
+++ src/components/text-base/text-base.tsx
@@ -6,18 +6,14 @@ import { Component, Prop, h, Host } from '@stencil/core';
   shadow: true,
 })
 export class TextBase {
-  // Propriedade para definir o tamanho: 'sm', 'md' ou 'lg'
   @Prop() size: 'sm' | 'md' | 'lg' = 'md';
-  
-  // Propriedade para definir a cor: 'default' (texto preto) ou 'inverse' (texto branco)
   @Prop() color: 'default' | 'inverse' = 'default';
 
   render() {
-    // Monta classes dinâmicas com base nas propriedades 'size' e 'color'
     const hostClasses = {
       'text-base': true,
-      [`text-${this.size}`]: true,     // Aplica text-sm, text-md ou text-lg
-      [`color-${this.color}`]: true,     // Aplica color-default ou color-inverse
+      [`text-${this.size}`]: true,
+      [`color-${this.color}`]: true,
     };
 
     return (
